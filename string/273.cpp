92bc0a4a641150ff45ba8672ba809d43cbdd6c0c
Cuitao <353615653@qq.com>
2022-02-19 14:29:39 +0800

    add 273

---
+++ string/273.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+#include <algorithm>
+#include <stack>
+
+using namespace std;
+
+
+class Solution {
+public:
+    string numberToWords(int num) {
+        // num转string
+        string s = std::to_string(num);
+        int sizeOfNum = s.length();
+    }
+};
+
+
+int main(){
+
+}
